ec20f30b3f1a4ed3d97a97f2db6e408735d029c3
AlassaneDIAW <alassanediaw0504@gmail.com>
2025-07-12 13:22:30 +0000

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/api', // adapte selon ton backend
+  baseURL: 'http://localhost:3000/api',
 });
 
 // Intercepteur pour ajouter le token
